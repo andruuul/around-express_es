d9b60f3b97058838b49a1f26e527f53bd9189b40
andruuul <andrescanulm@gmail.com>
2022-12-12 17:43:27 -0600

    create user model

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use('/cards', cardsRouter);
 app.use('/users', usersRouter);
